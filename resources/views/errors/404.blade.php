dae10ed0bc247d8f60235f01c4a58637fe7b519f
DELL <hpp@ubuntu.ubuntu-domain>
2019-11-07 10:07:24 -0500

    added testing.txt

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page not found - 404</title>
+</head>
+<body>
+ 	<div class="container">
+      <div class="row text-center">
+        <div class="col-lg-6 offset-lg-3 col-sm-6 offset-sm-3 col-12 p-3 error-main">
+          <div class="row">
+            <div class="col-lg-8 col-12 col-sm-10 offset-lg-2 offset-sm-1">
+              <h1 class="m-0">404</h1>
+              <h6>Page not found - ItSolutionStuff.com</h6>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</body>
+</html>
+
